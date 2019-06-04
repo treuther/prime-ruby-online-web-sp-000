37deaef6ffc44d94a304cf357d914795213f0d79
Tracy Reuther <tracyreuther@gmail.com>
2019-06-04 16:41:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,18 +2,17 @@
 def prime?(num)
   # check if num is less than 2.
   false if num < 2
-  
+
   # check if num is divisible by any number
   # between 2 to (num - 1)
   # return false on the first instance
   (2...num).each do |factor|
-    if (num % factor).zero? 
+    if (num % factor).zero?
       false
     elsif num < 2
       false
     end
   end
-  
+
   true # after checking all numbers and not receiving false
 end
-
