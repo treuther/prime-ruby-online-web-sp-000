2d95f5ab2ea6f371c0ee7e65014004ee882bbbdf
Tracy Reuther <tracyreuther@gmail.com>
2019-06-04 16:54:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,3 +11,15 @@ def prime?(number)
     false
   end
 end
+
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range..none? do |num_to_test|
+      number % num_to_test.zero?
+    end
+  else
+    false
+  end
+end
